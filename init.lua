d38c3f0714d5eaf5796d2d4d3a17c33109a9781f
feepwang <me@feep.wang>
2023-07-27 22:44:24 +0800

    initial repo

---
+++ init.lua
@@ -0,0 +1,10 @@
+require'plugins'
+require'options'
+
+vim.opt.number = true
+vim.opt.mouse = 'a'
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.expandtab = true
