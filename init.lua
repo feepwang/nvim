a298d62f19c65e968a738b05a4b6a3e246b7f54b
feepwang <me@feep.wang>
2023-07-29 21:54:44 +0800

    vim configure

--- init.lua
+++ init.lua
@@ -1,12 +1,23 @@
 require'plugins'
 
-vim.opt.number = true
-vim.opt.mouse = 'a'
-
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.smartindent = true
-vim.opt.expandtab = true
-
+-- display
 vim.opt.termguicolors = true
 vim.cmd('colorscheme kanagawa')
+
+vim.opt.wrap = false
+
+-- sidebar
+vim.opt.number = true
+vim.opt.numberwidth = 3
+vim.opt.mouse = 'a'
+
+-- tab/indent
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent =  true
+vim.opt.smartindent = true
+
+-- search/replace
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
