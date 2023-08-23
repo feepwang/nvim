ee551efb1b3ebadfb488a30330234966fd80bb6b
feepwang <me@feep.wang>
2023-08-23 22:17:22 +0800

    fix lspconfig

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -16,7 +16,7 @@ return {
 			local cmp = require('cmp')
 			local luasnip = require("luasnip")
 
-			cmp.setup {
+			cmp.setup({
 				snippet = {
 					expand = function(args)
 						luasnip.lsp_expand(args.body)
@@ -55,29 +55,20 @@ return {
 					{ name = 'luasnip' },
 					{ name = "buffer" },
 				},
-				sorting = {
-					comparators = {
-						cmp.config.compare.offset,
-						cmp.config.compare.exact,
-						cmp.config.compare.recently_used,
-						require("clangd_extensions.cmp_scores"),
-						cmp.config.compare.kind,
-						cmp.config.compare.sort_text,
-						cmp.config.compare.length,
-						cmp.config.compare.order,
-					}
-				},
-			}
+			})
 
 			-- lspconfig
-			local lspconfig = require("lspconfig")
 			local capabilities = require('cmp_nvim_lsp').default_capabilities()
-			-- c/c++
-			lspconfig.clangd.capabilities = capabilities
-			-- cmake
-			lspconfig.cmake.capabilities = capabilities
-			-- lua
-			lspconfig.lua_ls.capabilities = capabilities
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities
+			})
+	.		lspconfig.cmake.setup({
+				capabilities = capabilities
+			})
 		end
 	}
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,7 +2,13 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		lazy = true,
+		event = { "BufReadPre", "BufNewFile" },
 		config = function()
+			local lspconfig = require("lspconfig")
+			-- lspconfig.lua_ls.setup({})
+			lspconfig.clangd.setup({})
+			lspconfig.cmake.setup({})
+
 			-- Global mappings.
 			-- See `:help vim.diagnostic.*` for documentation on any of the below functions
 			vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
@@ -15,9 +21,6 @@ return {
 			vim.api.nvim_create_autocmd('LspAttach', {
 				group = vim.api.nvim_create_augroup('UserLspConfig', {}),
 				callback = function(ev)
-					-- Enable completion triggered by <c-x><c-o>
-					-- vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
 					-- Buffer local mappings.
 					-- See `:help vim.lsp.*` for documentation on any of the below functions
 					local opts = { buffer = ev.buf }
@@ -47,11 +50,22 @@ return {
 		dependencies = "neovim/nvim-lspconfig",
 		ft = "lua",
 		opts = {},
+		config = function()
+			require("lspconfig").lua_ls.setup({
+				settings = {
+					Lua = {
+						complettion = {
+							callSnippet = "Replace"
+						}
+					}
+				}
+			})
+		end
 	},
 	{
 		"p00f/clangd_extensions.nvim",
 		dependencies = "neovim/nvim-lspconfig",
 		ft = { "c", "cpp" },
-		opts = {},
+		opts = {}
 	}
 }
